d61efa7275bfdb486c48cb9a3088816ff90e22b5
aarysomane <somaneaary@gmail.com>
2025-09-09 20:22:44 +0530

    Chatbot added

---
+++ src/components/StudentPage.jsx
@@ -0,0 +1,26 @@
+import "./StudentPage.css";
+
+export default function StudentPage({ student }) {
+  const { studentName, studentId } = student || {};
+
+  return (
+    <div className="student-dashboard">
+      <div className="sidebar">
+        <ul className="sidebar-nav">
+        <li><span>Profile</span></li>
+  <li><span>Analysis</span></li>
+  <li><span>Attempt Quiz</span></li>
+  
+        </ul>
+      </div>
+
+      <main className="main-content">
+        <h1>Welcome Student !</h1>
+        <p>Overall Progress</p>
+      </main>
+      <div className="chatbot">
+    </div>
+    </div>
+    
+  );
+}
